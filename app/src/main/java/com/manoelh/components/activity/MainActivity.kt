92535e9fa3da99c6cce95ab011de74597eaf593b
Manoel Henrique <henriqe115@gmail.com>
2019-12-22 17:16:39 -0300

    spinner events, progressbar with screen lock and seekbar

--- app/src/main/java/com/manoelh/components/activity/MainActivity.kt
+++ app/src/main/java/com/manoelh/components/activity/MainActivity.kt
@@ -4,22 +4,23 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.ArrayAdapter
-import android.widget.TextView
-import android.widget.Toast
+import android.view.WindowManager
+import android.widget.*
 import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import com.google.android.material.snackbar.Snackbar
 import com.manoelh.components.R
 import com.manoelh.components.mock.Mock
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity(), View.OnClickListener, AdapterView.OnItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setListeners()
         loadSpinner()
+
     }
 
     override fun onClick(view: View) {
@@ -28,11 +29,21 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             showToast()
         else if (id == R.id.buttomSnackbar)
             showSnackbar()
+        else if (id == R.id.buttonGetSpinner)
+            getValueSpinner()
+        else if (id == R.id.buttonSetSpinner)
+            setValueSpinner()
+        else if (id == R.id.buttonProgress)
+            changeVisibilityProgressBar()
     }
 
     private fun setListeners(){
         buttonToast.setOnClickListener(this)
         buttomSnackbar.setOnClickListener(this)
+        buttonGetSpinner.setOnClickListener(this)
+        buttonSetSpinner.setOnClickListener(this)
+        spinnerDynamic.onItemSelectedListener = this
+        buttonProgress.setOnClickListener(this)
     }
 
     private fun showToast(){
@@ -53,9 +64,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         snackbar.view.findViewById<TextView>(com.google.android.material.R.id.snackbar_text).text = "Snackbar custom"
         snackbar.view.findViewById<TextView>(com.google.android.material.R.id.snackbar_text).setTextColor(Color.WHITE)
         snackbar.view.setBackgroundColor(Color.GRAY)
-        snackbar.view.setBackgroundColor(ContextCompat.getColor(applicationContext,
-            R.color.colorPrimaryDark
-        ))
+        snackbar.view.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.colorPrimaryDark))
         snackbar.setAction("Undo") {
             Snackbar.make(constraintLayout, "Undone", Snackbar.LENGTH_LONG).show()
         }
@@ -67,5 +76,39 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         val animals = Mock.getListAnimals()
         val adapter = ArrayAdapter(applicationContext, android.R.layout.simple_spinner_dropdown_item, animals)
         spinnerDynamic.adapter = adapter
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+    }
+
+    private fun getValueSpinner(){
+        var  spinnervalue = spinnerDynamic.selectedItem
+        Toast.makeText(applicationContext, "get item: $spinnervalue", Toast.LENGTH_LONG).show()
+    }
+
+    private fun setValueSpinner(){
+        spinnerDynamic.setSelection(1)
+    }
+
+    private fun changeVisibilityProgressBar(){
+        if (progressBar.isVisible){
+            progressBar.visibility = ProgressBar.INVISIBLE
+            buttonProgress.text = getString(R.string.progress)
+        }
+        else{
+            progressBar.visibility = ProgressBar.VISIBLE
+            buttonProgress.text = getString(R.string.progressStop)
+            window.setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
+            textViewScreenEnable.visibility = TextView.VISIBLE
+        }
+    }
+
+    override fun onBackPressed(){
+        window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
+        textViewScreenEnable.visibility = TextView.INVISIBLE
     }
 }
